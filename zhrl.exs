18f658f29bb0fcbf7801eec952d9d357be4ab845
JasminePatel <jpatel26@calpoly.edu>
2019-03-03 16:29:54 -0800

    Adding working

--- zhrl.exs
+++ zhrl.exs
@@ -4,6 +4,12 @@
     defstruct value: -1
   end
 
+  # IdC ExprC Struct Definition -- Add more ExprC later
+  defmodule IdC do
+    defstruct value: -1
+  end
+
+
   # NumC Value Struct Definition -- Add more Value later
   defmodule NumV do
     defstruct value: -1
@@ -15,26 +21,26 @@
   end
 
   # A PrimV
-  defmodule PrimV do
+  defmodule PrimC do
     defstruct operator: 0, function: 0
   end
-
-
-
 
 
 defmodule Main do
 
   def parse(n) do
-    num = %NumC{value: n}
-    num
+    cond do
+      is_integer(n) -> %NumC{value: n}
+      is_atom(n) -> %IdC{value: n}
+    end
     # IO.puts num.value #Printing
   end
 
-  # given ExprC, output a Value
-  def interp(e) do
-    case e do
-      %NumC{} -> %NumV{value: e.value}
+  # given ExprC and environment, output a Value
+  def interp(expr, env) do
+    case expr do
+      %NumC{} -> %NumV{value: expr.value}
+      %IdC{} -> envlookup(env, expr.value)
     end
   end
 
@@ -42,6 +48,7 @@ defmodule Main do
   def serialize(v) do
     case v do
       %NumV{} -> IO.puts v.value
+      'myadd' -> IO.puts 'matched myadd'
     end
   end
 
@@ -52,13 +59,27 @@ defmodule Main do
     end
   end
 
+  def extend_env(env, sym, val) do
+    Map.put(env, sym, val)
+  end
+
+  def topEnv() do
+    te = extend_env(%Environment{}.bindings, :+, 'myadd')
+    te
+  end
+
+  def envlookup(env, sym) do
+    Map.get(env, sym)
+  end
+
+
+
   # given s, evaluate the s
   def topInterp(s) do
-    extend_env = fn (env, sym, val) -> Map.put(env, sym, val) end
-    extend_env.(%Environment{}.bindings, :+, :myadd)
-    serialize(interp(parse(s)))
+    serialize(interp(parse(s), topEnv()))
     myadd(%NumC{value: 3}, %NumC{value: 3})
   end
 end
 
 IO.puts Main.topInterp(8)
+Main.topInterp(:+)
